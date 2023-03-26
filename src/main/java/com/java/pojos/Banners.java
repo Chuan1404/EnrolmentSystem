3a298947cafde8f194de00a4462cf0d777c4862e
nguyencuong0311 <2051052016cuong@ou.edu.vn>
2023-03-27 04:33:41 +0700

    add pojo classes for 3 new tables

---
+++ src/main/java/com/java/pojos/Banners.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.java.pojos;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author jackc
+ */
+@Entity
+@Table(name = "banners")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Banners.findAll", query = "SELECT b FROM Banners b"),
+    @NamedQuery(name = "Banners.findById", query = "SELECT b FROM Banners b WHERE b.id = :id")})
+public class Banners implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "bannerId")
+    private Collection<Images> imagesCollection;
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "bannerId")
+    private Homepage homepage;
+
+    public Banners() {
+    }
+
+    public Banners(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @XmlTransient
+    public Collection<Images> getImagesCollection() {
+        return imagesCollection;
+    }
+
+    public void setImagesCollection(Collection<Images> imagesCollection) {
+        this.imagesCollection = imagesCollection;
+    }
+
+    public Homepage getHomepage() {
+        return homepage;
+    }
+
+    public void setHomepage(Homepage homepage) {
+        this.homepage = homepage;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Banners)) {
+            return false;
+        }
+        Banners other = (Banners) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.java.pojos.Banners[ id=" + id + " ]";
+    }
+    
+}
